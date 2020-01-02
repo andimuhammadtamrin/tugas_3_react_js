2377c675cb4a6f7d06cdb6805630c698e12ba22f
Andi Muhammad Tamirn <andimuhammadtamrin@gamil.com>
2020-01-02 22:55:49 +0800

    Tugas 3 React JS
    Tugas ini saya kumpulkan untuk NIomic

---
+++ src/Page/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render(){
+    return(
+      <center>
+        <b>
+          <td>Home |</td>
+          <td>Product |</td>
+          <td>Kontak |</td>
+          <td>Tentang Kami</td>
+        </b>
+      </center>
+    );
+  }
+}
+export default Header;
